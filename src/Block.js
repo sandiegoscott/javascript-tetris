688198666097905c54ccab3f01d947bd7036a115
Scott Olmsted <solmsted@gmail.com>
2016-06-23 13:09:01 -0400

    Debug Canvas and Block functions

--- src/Block.js
+++ src/Block.js
@@ -2,15 +2,24 @@ function Block(canvas, row, col, color) {
   //
   // a block for each cell
   //
-  this.canvas_el = canvas.el;
+  this.canvas = canvas;
   this.row = row;
   this.col = col;
+  console.log('row=', row, '  col=', col)
+  this.color = color;
   this.el = document.createElement('div');
-  this.canvas_el.appendChild(this.el); 
-
+  this.setStyle();
+  this.canvas.el.appendChild(this.el); 
 }
 
-Block.prototype.setStyle = function(css) {
-  // 'height: 25px; width: 25px; border: 1px solid #333; background-color: blue' 
-  this.el.setAttribute('style', css + ' background-color: ' + this.color);
+Block.prototype.setStyle = function() {
+  var style = '';
+  style += 'top: ' + this.row * this.canvas.block_height + 'px; ';
+  style += 'left: ' + this.col * this.canvas.block_width + 'px; ';
+  style += 'height: ' + this.canvas.block_height + 'px; ';
+  style += 'width: ' + this.canvas.block_width + 'px; ';
+  style += 'border: 1px solid #333; background-color: ' + this.color + ';';
+  style += 'position: absolute';
+  this.el.setAttribute('style', style);
+  //debugger;
 }
--- src/Canvas.js
+++ src/Canvas.js
@@ -5,32 +5,15 @@ function Canvas(id, rows, cols) {
   this.el = get(id);
   this.rows = rows;
   this.cols = cols;
+  this.setDimensions();
   this.blocks = [ ];
-  this.setupDimensions();
 }
 
-Canvas.prototype.setupDimensions = function() {
+Canvas.prototype.setDimensions = function() {
   // set dims to physical dims
   this.height = this.el.offsetHeight;
   this.width = this.el.offsetWidth;
   // compute size of blocks
   this.block_height = this.height / this.rows;
-  this.block_width = this.width / this.rows;
+  this.block_width = this.width / this.cols;
 }
-
-// add cells of piece to occupied cells
-Canvas.prototype.createBlock = function() {
-  var canvas = get('canvas');
-
-  var block = document.createElement('div');
-  block.setAttribute('id', 'block');
-  block.setAttribute('style', 'height: 25px; width: 25px; border: 1px solid #333; background-color: blue');
-  //debugger;
-  canvas.appendChild(block); 
-}
-
-    function drawBlock(ctx, x, y, color) {
-      ctx.fillStyle = color;
-      ctx.fillRect(x*dx, y*dy, dx, dy);
-      ctx.strokeRect(x*dx, y*dy, dx, dy)
-    }
