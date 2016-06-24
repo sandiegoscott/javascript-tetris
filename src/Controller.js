5c6f9a4c0291ab42899e730bad0fcc03b7982ff9
Scott Olmsted <solmsted@gmail.com>
2016-06-24 11:50:28 -0400

    Fix bugs in HTML and Controller

--- src/Controller.js
+++ src/Controller.js
@@ -1,18 +1,20 @@
 function start() {
   var i;
 
-  canvas = new Canvas('canvas', 20, 10);
-  upcoming = new Canvas('upcoming', 6, 6);
+  field_el = document.getElementById('field');
+  on_deck_el = document.getElementById('on_deck');
+  field = new Canvas(field_el, 20, 10);
+  on_deck = new Canvas(on_deck_el, 6, 6);
   
   for (i = 0; i < 16; i += 1) {
-    canvas.addBlock(i, Math.floor(i/2), 'aqua');
-    canvas.addBlock(i, Math.floor(i/2)+1, 'yellow');
-    canvas.addBlock(i, Math.floor(i/2)+2, 'springgreen');
+    field.addBlock(i, Math.floor(i/2), 'aqua');
+    field.addBlock(i, Math.floor(i/2)+1, 'yellow');
+    field.addBlock(i, Math.floor(i/2)+2, 'springgreen');
   }
   for (j = 0; j < 6; j += 1) {
-    upcoming.addBlock(j, Math.floor(j/2), 'aqua');
-    upcoming.addBlock(j, Math.floor(j/2)+1, 'yellow');
-    upcoming.addBlock(j, Math.floor(j/2)+2, 'springgreen');
+    on_deck.addBlock(j, Math.floor(j/2), 'aqua');
+    on_deck.addBlock(j, Math.floor(j/2)+1, 'yellow');
+    on_deck.addBlock(j, Math.floor(j/2)+2, 'springgreen');
   }
 
 }
